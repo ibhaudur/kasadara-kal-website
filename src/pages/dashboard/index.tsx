8a558cce2003ab8ac96e3037b45e3a79db7a94c6
Samuel-Thangarasu <144303704+Samuel-Thangarasu@users.noreply.github.com>
2025-06-29 20:42:01 +0530

    Rank card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import medal from '../../../public/images/medal.svg';
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="min-h-screen bg-white p-6 font-sans ">
+       <header className="mb-8 flex justify-between items-center">
+        <h1 className="text-3xl text-light">
+          Welcome back, <span className="text-3xl  font-bold">John Smith</span>
+          <p className="text-sm text-gray-500">Here you can see your overall rank and activities</p>
+        </h1>
+      
+      </header>
 
-export default Dashboard
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8 ">
+         
+        <div className="relative p-6 rounded-xl shadow w-full max-w-sm" >
+
+         <img src={medal} alt="Medal Icon" className=" absolute top-0 right-5 w-20 h-20 -mt-0 -mr-0" />
+          <p className="text-gray-600 mb-3 ">Rank</p>
+          <h2 className="text-3xl font-bold ">
+            23 <span className="text-gray-500 text-sm">/ 50</span>
+                  <p className="text-sm text-gray-500 mt-2 "> Compared to all the attendees who attend the same exams as you.</p>
+          </h2>
+        </div>
+         </section>
+    </div>
+  );
+};
+
+export default Dashboard;
+  
